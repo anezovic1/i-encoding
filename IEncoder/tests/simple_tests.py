70a725e5de31455a501431a6d424dcd20c6c4a49
Anida Nezovic <anezovic1@etf.unsa.ba>
2024-10-15 20:53:42 +0200

    one-hot encoding vs i encoding

--- IEncoder/tests/simple_tests.py
+++ IEncoder/tests/simple_tests.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from IEncoder.i_encoder import IEncoder
 
-# first example
+########################### first example ########################### 
 data = pd.DataFrame({
     'color': ['red', 'blue', 'green', 'blue', 'red'],
     'size': ['S', 'M', 'L', 'M', 'S'],
@@ -16,7 +16,7 @@ print("data that is i encoded:\n", transformed)
 inversed = encoder.inverse_transform(transformed)
 print("inverse data:\n", inversed)
 
-# second example
+########################### second example ########################### 
 data_new = {'Employee id': [10, 20, 15, 25, 30],
         'Gender': ['M', 'F', 'F', 'M', 'F'],
         'Remarks': ['Good', 'Nice', 'Good', 'Great', 'Nice'],
@@ -31,7 +31,7 @@ print("employee data:\n", df)
 transformed = encoder.fit_transform(df)
 print("data that is i encoded:\n", transformed)
 
-# third example
+########################### third example ########################### 
 data_new_2 = pd.DataFrame({'Employee id': [10, 20, 15, 25, 30],
         'Gender': ['M', 'F', 'F', 'M', 'F'],
         'Remarks': ['Good', 'Nice', 'Good', 'Great', 'Nice'],
@@ -45,3 +45,18 @@ print("data that is i encoded:\n", transformed)
 
 inversed = encoder.inverse_transform(transformed)
 print("inverse data:\n", inversed)
+
+########################### fourth example ########################### 
+from sklearn.preprocessing import OneHotEncoder
+
+data_new_3 = pd.DataFrame({'Category': ['A', 'B', 'C', 'A', 'B']})
+
+one_hot_encoder = OneHotEncoder(sparse_output=False)
+one_hot_encoded = one_hot_encoder.fit_transform(data_new_3[['Category']])
+encoded_df = pd.DataFrame(one_hot_encoded, columns=one_hot_encoder.get_feature_names_out(['Category']))
+print("one-hot encoded data:\n", encoded_df)
+
+i_encoder = IEncoder()
+i_encoded = encoder.fit_transform(data_new_3[['Category']])
+i_encoded_df = pd.DataFrame(i_encoded, columns=i_encoder.get_feature_names_out(['Category']))
+print("i encoded data:\n", i_encoded_df)
